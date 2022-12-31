4cfa0d45b209c6008ac45cf7fb4fd99571a66604
jykwon <jykwon@12cm.co.kr>
2022-12-31 15:19:50 +0900

    feat(Chapter01 : 자바 시작하기) - 코드 용어 이해 (1.10), 코드 주석 달기 (1.11)

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,13 @@
+package ch01.sec09;
+
+/**
+ * 도큐먼트 주석 : javadpc 명령어로 API 도큐먼트를 생성하는 데 사용
+ * @author jykwon
+ */
+public class Hello {
+    // 프로그램 실행 진입점
+    public static void main(String[] args) {
+        // 콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+    }
+}
