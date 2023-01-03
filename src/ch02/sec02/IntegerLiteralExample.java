1ee13db96a990dd4f2d6af18b3393abd857de4ec
jykwon <jykwon@12cm.co.kr>
2023-01-03 22:24:11 +0900

    feat(Chapter02 : 변수와 타입) - 정수 타입 (2.2)

---
+++ src/ch02/sec02/IntegerLiteralExample.java
@@ -0,0 +1,23 @@
+package ch02.sec02;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+
+        // 2진수
+        int val1 = 0b1011;
+
+        // 8진수
+        int val2 = 0206;
+
+        // 10진수
+        int val3 = 365;
+
+        // 16진수
+        int val4 = 0xB3;
+
+        System.out.println("val1 = " + val1);
+        System.out.println("val2 = " + val2);
+        System.out.println("val3 = " + val3);
+        System.out.println("val4 = " + val4);
+    }
+}
