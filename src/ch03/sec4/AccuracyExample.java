80137cef77fc30c1b4210648583a1a1fdb1aaf4e
jykwon <jykwon@12cm.co.kr>
2023-02-13 20:41:11 +0900

    feat(Chapter03 : 연산자) - 정확한 계산은 정수 연산으로(3.4)

---
+++ src/ch03/sec4/AccuracyExample.java
@@ -0,0 +1,15 @@
+package ch03.sec4;
+
+public class AccuracyExample {
+
+    public static void main(String[] args) {
+
+        int apple = 1;
+        double pieceUnit = 0.1;
+        int number = 7;
+
+        double result = apple - number * pieceUnit;
+
+        System.out.println("result = " + result);
+    }
+}
