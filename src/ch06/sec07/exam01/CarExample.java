a61f4bcc3c28e768f3516b53c2520e3d9d828ecb
kwonjiyoon <yoon980112@naver.com>
2024-01-03 21:54:19 +0900

    feat(chapter06) : 클래스

---
+++ src/ch06/sec07/exam01/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec07.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+
+        Car myCar = new Car("그랜저", "검정", 250);
+
+        // 기본 생성자는 호출하지 못함
+        // Car myCar = new Car();
+    }
+}
